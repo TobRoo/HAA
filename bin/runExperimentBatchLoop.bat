a6600da49c0005438beb18e1cdb9a42994336257
Tobias Roos <tobias.roos@ltu.se>
2017-04-12 10:36:52 +0200

    Simulation run scheduling imrpovements

--- bin/runExperimentBatchLoop.bat
+++ bin/runExperimentBatchLoop.bat
@@ -1,11 +1,12 @@
 
 FOR /L %%A IN (1,1,300) DO (
 
-start "hostExclusive" %~dp0\Autonomic.exe hostCfgs\hostExclusive.cfg %%A %%A
+
 start "hostLocal1" %~dp0\Autonomic.exe hostCfgs\hostLocal1.cfg %%A %%A data\missions\missionRCISLExperiment2HighFailure.ini
 start "hostLocal2" %~dp0\Autonomic.exe hostCfgs\hostLocal2.cfg %%A %%A
 start "hostLocal3" %~dp0\Autonomic.exe hostCfgs\hostLocal3.cfg %%A %%A
-start /wait "hostLocal4" %~dp0\Autonomic.exe hostCfgs\hostLocal4.cfg %%A %%A
+start "hostLocal4" %~dp0\Autonomic.exe hostCfgs\hostLocal4.cfg %%A %%A
+start /wait "hostExclusive" %~dp0\Autonomic.exe hostCfgs\hostExclusive.cfg %%A %%A
 
 ECHO Current run number is: %%A
 )
--- bin/runExperimentBatchLoopHighFail.bat
+++ bin/runExperimentBatchLoopHighFail.bat
@@ -1,11 +1,12 @@
 
 FOR /L %%A IN (1,1,300) DO (
 
-start "hostExclusive" %~dp0\Autonomic.exe hostCfgs\Experiment2RCISLHighFail\hostExclusive.cfg %%A %%A
+
 start "hostLocal1" %~dp0\Autonomic.exe hostCfgs\Experiment2RCISLHighFail\hostLocal1.cfg %%A %%A data\missions\missionRCISLExperiment2HighFailure.ini
 start "hostLocal2" %~dp0\Autonomic.exe hostCfgs\Experiment2RCISLHighFail\hostLocal2.cfg %%A %%A
 start "hostLocal3" %~dp0\Autonomic.exe hostCfgs\Experiment2RCISLHighFail\hostLocal3.cfg %%A %%A
-start /wait "hostLocal4" %~dp0\Autonomic.exe hostCfgs\Experiment2RCISLHighFail\hostLocal4.cfg %%A %%A
+start "hostLocal4" %~dp0\Autonomic.exe hostCfgs\Experiment2RCISLHighFail\hostLocal4.cfg %%A %%A
+start /wait "hostExclusive" %~dp0\Autonomic.exe hostCfgs\Experiment2RCISLHighFail\hostExclusive.cfg %%A %%A
 
 ECHO Current run number is: %%A
 )
